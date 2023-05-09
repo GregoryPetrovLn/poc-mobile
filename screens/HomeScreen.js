fa82e3d22a275966dac7807f1d6fbdc0f192b813
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-09 12:02:10 +0300

    added table to homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,34 @@
-import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect } from "react";
 import { SafeAreaView, View } from "react-native";
-import { Text } from "react-native-elements";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import tw from "tailwind-react-native-classnames";
+import Table from "../components/Table";
+import {
+  getListProducts,
+  setSelectedProductId,
+} from "../slices/products/actions";
+import { selectProduct } from "../slices/products/productSlice";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
+  const { list, loading } = useSelector(selectProduct);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    dispatch(getListProducts());
+  }, []);
+
+  const onRowPress = (item) => {
+    navigation.navigate("ProductScreen");
+    console.log("onRowPress", item);
+    dispatch(setSelectedProductId(item._id));
+  };
 
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
-        <Text>Hello</Text>
+        <Table data={list} onRowPress={onRowPress} />
       </View>
     </SafeAreaView>
   );
